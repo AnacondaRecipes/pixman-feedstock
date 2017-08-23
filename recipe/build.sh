7a627b5bd156aec17c69dcfe5fbf3cc3f9c9a5b9
Ray Donnelly <mingw.android@gmail.com>
2017-08-23 12:26:38 +0100

    make in parallel and bump build number

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,12 @@ fi
 
 ./configure --prefix=$PREFIX $OPTS
 
+<<<<<<< HEAD
 make -j$CPU_COUNT
 make check -j$CPU_COUNT
 make install -j$CPU_COUNT
+=======
+make -j${CPU_COUNT}
+make check
+make install
+>>>>>>> make in parallel and bump build number
